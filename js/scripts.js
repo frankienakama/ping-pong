1d6650a130173dfad4486a42cbc37cc751e56a52
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-20 11:36:56 -0800

    make some changes to js

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,11 @@
 
 //front-end
 $(document).ready(function() {
-	$("#english").submit(function(e) {
-  	e.preventDefault();  // none of your BS, form
-    var word = $("#string").val();  // get the word from the input
-    var result = isLatin(word);  // call `isLatin` with the word
-    $("#pig").text(result);
-  });
+  $("#input").submit(function(event) {
+    event.preventDefault();
+    result = $("#user-input").val();
+    $("#result").text(result);
+  })
 });
 
 //variables:
