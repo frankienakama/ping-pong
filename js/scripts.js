014d4f0d58d296181de8d7e2d481e7c77dd6bc51
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-20 08:57:50 -0800

    remove some jquery, was getting too confusing

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,13 @@
 
 //front-end
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault();
-    userInput = $("#user-input").val();
-    var result = (userInput); //posibly change the variable here? confusing
-    $("#returnNumList").text(result);
-  })
+
+  });
 });
 
 
 //variables:
 //form id="enterNum"
-//input id="user-input"
+//input id="userNumInput"
 //span id="numList"
-//input function's result to display="gameResults"
 //
