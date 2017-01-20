e480525e01592fbefb563e8b7264136b06ec5a09
Sara Frandsen <sara.frandsen@gmail.com>
2017-01-20 12:23:49 -0800

    work on changing 5s to pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 //back-end
 var countingArray = function(input) {
     var result = [];
-        for(var i=1; i<=input; i++)
-        if (i % 3 === 0) {
-          result.push("Ping!");
-        } else {
-          result.push(i);
-        }
+        for(var i=1; i<=input; i++) {
+          if(i % 5 === 0) {
+            result.push("Pong!");
+            } else if {
+            (i % 3 === 0) {
+            result.push("Ping!");
+            } else {
+            result.push(i);
+          }
     return result;
 }
 
